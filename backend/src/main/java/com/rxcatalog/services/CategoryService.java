1924f8d5d0c4fcab5e319c72cc238ca1eed6c842
Roger Pereira <rogerpsantos3@gmail.com>
2021-11-18 20:59:26 -0300

    Add informações no DataBase H2 | Testes

--- backend/src/main/java/com/rxcatalog/services/CategoryService.java
+++ backend/src/main/java/com/rxcatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rxcatalog.entities.Category;
 import com.rxcatalog.repositories.CategoryRepository;
@@ -11,10 +12,10 @@ import com.rxcatalog.repositories.CategoryRepository;
 @Service
 public class CategoryService {
 	
-//	@SuppressWarnings("unused")
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
