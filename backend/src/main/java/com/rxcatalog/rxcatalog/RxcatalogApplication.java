4d4f1ea865da9b331dd618bae9158b18dc2dbf16
Roger Pereira <rogerpsantos3@gmail.com>
2021-11-17 21:47:19 -0300

    Project Created

---
+++ backend/src/main/java/com/rxcatalog/rxcatalog/RxcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.rxcatalog.rxcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rxcatalog/rxcatalog/RxcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rxcatalog.rxcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
